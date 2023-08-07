3b01cdc69ab624e65d30c06138e755190b47c9e8
pintor.dev <pintor.dev@gmail.com>
2023-08-07 12:54:49 +0900

    todo #3

--- src/main/java/com/pintor/marketapp/product/controller/ProductController.java
+++ src/main/java/com/pintor/marketapp/product/controller/ProductController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,12 +19,20 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String list(Model model) {
 
         List<Product> productList = this.productService.getList();
         model.addAttribute("productList", productList);
 
         return "product/list";
+    }
+
+    @GetMapping("{id}")
+    public String detail(Model model, @PathVariable("id") Long id) {
+
+        Product product = this.productService.getProduct(id);
+
+        return "product/detail";
     }
 }
--- src/main/java/com/pintor/marketapp/product/service/ProductService.java
+++ src/main/java/com/pintor/marketapp/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.pintor.marketapp.product.service;
 
+import com.pintor.marketapp.DataNotFoundException;
 import com.pintor.marketapp.product.entity.Product;
 import com.pintor.marketapp.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +32,15 @@ public class ProductService {
     public List<Product> getList() {
 
         return this.productRepository.findAll();
+    }
+
+    public Product getProduct(Long id) {
+
+        Optional<Product> _product = this.productRepository.findById(id);
+        if (_product.isPresent()) {
+            return _product.get();
+        } else {
+            throw new DataNotFoundException("product not found");
+        }
     }
 }
