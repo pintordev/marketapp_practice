107ed6148157a345493010c15a55b26c32b75f68
pintor.dev <pintor.dev@gmail.com>
2023-08-08 10:28:37 +0900

    security_config

--- src/main/java/com/pintor/marketapp/market/repository/MarketRepository.java
+++ src/main/java/com/pintor/marketapp/market/repository/MarketRepository.java
@@ -2,6 +2,8 @@ package com.pintor.marketapp.market.repository;
 
 import com.pintor.marketapp.market.entity.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
 }
--- src/main/java/com/pintor/marketapp/product/repository/ProductRepository.java
+++ src/main/java/com/pintor/marketapp/product/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.pintor.marketapp.product.repository;
 
 import com.pintor.marketapp.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
