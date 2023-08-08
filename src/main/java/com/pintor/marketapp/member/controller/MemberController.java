e702de6aad45a31b197bc3f79455624206b3e8ae
pintor.dev <pintor.dev@gmail.com>
2023-08-08 09:12:12 +0900

    login & join controller open

--- src/main/java/com/pintor/marketapp/member/controller/MemberController.java
+++ src/main/java/com/pintor/marketapp/member/controller/MemberController.java
@@ -1,7 +1,25 @@
 package com.pintor.marketapp.member.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/member")
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
+
+    @GetMapping("/login")
+    @ResponseBody
+    public String login() {
+        return "로그인 페이지 입니다.";
+    }
+
+    @GetMapping("/join")
+    @ResponseBody
+    public String join() {
+        return "회원가입 페이지 입니다.";
+    }
 }
--- src/test/java/com/pintor/marketapp/service/ProductServiceTests.java
+++ src/test/java/com/pintor/marketapp/service/ProductServiceTests.java
@@ -12,16 +12,16 @@ public class ProductServiceTests {
     @Autowired
     private ProductService productService;
 
-    @Test
-    @DisplayName("강의 제품 생성")
-    void createProductTest() {
-
-        for (int i = 2; i <= 200; i++) {
-
-            String name = String.format("테스트 상품 [%03d]", i);
-            int price = 1;
-
-            this.productService.create(name, price);
-        }
-    }
+//    @Test
+//    @DisplayName("강의 제품 생성")
+//    void createProductTest() {
+//
+//        for (int i = 2; i <= 200; i++) {
+//
+//            String name = String.format("테스트 상품 [%03d]", i);
+//            int price = 1;
+//
+//            this.productService.create(name, price);
+//        }
+//    }
 }
